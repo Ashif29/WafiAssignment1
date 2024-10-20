95f49bf157291539cd463e7a303113f35ec6ee5a
Asif <asif.duet.cse19@gamil.com>
2024-10-20 14:48:03 +0600

    Added Serilog

---
+++ WafiArche.Api/Products/ProductController.cs
@@ -0,0 +1,79 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WafiArche.Application.Products;
+using WafiArche.Application.Products.Dtos;
+using WafiArche.Domain.Products;
+
+namespace WafiArche.Api.Controllers
+{
+    //[Route("api/[controller]")]
+    //[ApiController]
+    //public class ProductController : ControllerBase
+    //{
+    //    private readonly IProductAppService _productAppService;
+    //    private readonly IMapper _mapper;
+    //    public ProductController(IProductAppService productAppService, IMapper mapper)
+    //    {
+    //        _productAppService = productAppService;
+    //        _mapper = mapper;
+    //    }
+
+    //    [HttpPost]
+    //    public ActionResult<ProductDto> CreateProduct([FromBody] ProductCreateDto productCreateDto)
+    //    {
+    //        Product product = _mapper.Map<Product>(productCreateDto);
+    //        var alpha = _productAppService.CreateProduct(product);
+    //        return Ok(alpha);
+    //    }
+
+    //    [HttpGet]
+    //    public ActionResult<IEnumerable<ProductDto>> GetAllProducts()
+    //    {
+    //        IEnumerable<Product> productList = _productAppService.GetAll();
+
+    //        return Ok(_mapper.Map<List<ProductDto>>(productList));
+    //    }
+    //}
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductAppService _productAppService;
+        private readonly IMapper _mapper;
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(IProductAppService productAppService, IMapper mapper, ILogger<ProductController> logger)
+        {
+            _productAppService = productAppService;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public ActionResult<ProductDto> CreateProduct([FromBody] ProductCreateDto productCreateDto)
+        {
+            _logger.LogInformation("Received request to create a product");
+
+            Product product = _mapper.Map<Product>(productCreateDto);
+            var alpha = _productAppService.CreateProduct(product);
+
+            _logger.LogInformation("Product created successfully with ID: {ProductId}", alpha.Id);
+
+            return Ok(alpha);
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ProductDto>> GetAllProducts()
+        {
+            _logger.LogInformation("Received request to fetch all products");
+
+            IEnumerable<Product> productList = _productAppService.GetAll();
+
+            _logger.LogInformation("Fetched {ProductCount} products", productList.Count());
+
+            return Ok(_mapper.Map<List<ProductDto>>(productList));
+        }
+    }
+
+}
--- WafiArche.Api/Program.cs
+++ WafiArche.Api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 using WafiArche.Application.Mappings;
 using WafiArche.Application.Products;
 using WafiArche.EntityFrameworkCore.Data;
@@ -12,6 +13,15 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 );
 // Register AutoMapper
 builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+// Configure Serilog for logging
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 builder.Services.AddScoped<IProductAppService, ProductAppService>();
 
