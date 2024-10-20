582dc27f224a89dec89a36fa08cbfc56d9ac5f39
Asif <asif.duet.cse19@gamil.com>
2024-10-20 13:54:28 +0600

    Added Product Controller

---
+++ WafiArche.Api/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WafiArche.Application.Products;
+using WafiArche.Domain.Products;
+
+namespace WafiArche.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductAppService _productAppService;
+        public ProductController(IProductAppService productAppService)
+        {
+            _productAppService = productAppService;
+        }
+
+        [HttpPost]
+        public ActionResult<Product> CreateProduct(Product product)
+        {
+            var alpha = _productAppService.CreateProduct(product);
+
+            return Ok(alpha);
+        }
+    }
+}
